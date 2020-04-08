a568fb32b79b2fe231020ce85d3faffade987468
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-04-07 19:55:17 -0600

    Add section others and news images

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,13 +1,12 @@
-      // Importaciones
-      // import {error} from './youtube-video'
-
 
       // Variables
 
       const cards = document.getElementById('card-container')
+      const cards_others = document.getElementById('card-container-others')
       const ele = document.getElementsByClassName('user-name')
       const videoContainer = document.getElementById('video')
       const form_video = document.getElementById('form-video')
+      const category = document.getElementsByClassName('categoryImages')
       // const BASE_URL = `static/images/${i}`
 
       function renderTemplate(url) {
@@ -24,7 +23,7 @@
                 <div class="user-card">
                   <img loading="lazy" class="user-img" src="https://scontent.fmga3-2.fna.fbcdn.net/v/t1.0-1/c38.0.160.160a/p160x160/86699303_832721240486433_5068736281786187776_o.jpg?_nc_cat=109&_nc_sid=dbb9e7&_nc_ohc=wwMxlVUmKrIAX_Y4eCt&_nc_ht=scontent.fmga3-2.fna&oh=d753d59c0bc1bc3b3453e349edd14edf&oe=5EAD5ADD"
                    alt="Foto del usuario">
-                   <p class="user-name">Walter Solorz...  </p>
+                   <p class="user-name">Walter Solorzano</p>
                 </div>
               <div>
                   <button class="btn-user blue">add image</button>
@@ -37,24 +36,39 @@
       }
 
       document.addEventListener('DOMContentLoaded', () => {
+        
         for(var url=1; url<=27; url++) {
           const HTMLString = renderTemplate(url)
           const html = document.implementation.createHTMLDocument();
           html.body.innerHTML = HTMLString;
           cards.append(html.body.children[0])
-        //   console.log(name[url])
+          //   console.log(name[url])
         }
 
+        for(var url=28; url<=80; url++) {
+          const HTMLString = renderTemplate(url)
+          const html = document.implementation.createHTMLDocument();
+          html.body.innerHTML = HTMLString;
+          cards_others.append(html.body.children[0])
+          //   console.log(name[url])
+        }
+        
         // setTimeout(() => {
-        //     for (let i = 1; i <= 27 ; i++) {
-        //         console.log(i)
-        //         if(i <=5) {
-        //             alert()
-        //         }
-        //     }
-        // },4000)
-        M.AutoInit()
-    })
+          //     for (let i = 1; i <= 27 ; i++) {
+            //         console.log(i)
+            //         if(i <=5) {
+              //             alert()
+              //         }
+              //     }
+              // },4000)
+            M.AutoInit()
+
+       })
+       debugger
+       window.addEventListener('load', () => {
+        
+       })
+
 
     // async function hello() {
     //   const api = await fetch('https://miguelhg2351.github.io/albumDeFotos/public/static/js/index.json')
