8e6b7515446128b9f3ebd9f167563ab5ec132566
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2020-11-28 07:40:44 -0600

    default webp |> old site

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Card({image, description}) {
+const Card = styled.div`
+    margin-top: 25px;
+    border: 1px solid red;
+    & .header-card {
+        & img {
+            max-width: 100%;
+            object-fit: cover;
+            vertical-align: middle;
+        }
+    }
+`;
+
+export default function FnCard({ image, description }) {
     return (
-        <div className="card">
-            <div className="header-card">
-                <img src={image} style={{width: '250px'}} loading="lazy" alt={description}/>
-            </div>
-            <div className="description-card"></div>
-            <div className="footer-card"></div>
-        </div>
+        <>
+            <Card>
+                <div className="header-card">
+                    <img
+                        src={image}
+                        loading="lazy"
+                        alt={description}
+                    />
+                </div>
+                <div className="description-card"></div>
+                <div className="footer-card"></div>
+            </Card>
+        </>
     );
 }
