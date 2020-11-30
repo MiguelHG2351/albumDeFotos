7c00c55f7ded18e269ff8c28b9d5822628be63f9
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2020-11-30 07:07:02 -0600

    data

--- src/components/allPhoto.js
+++ src/components/allPhoto.js
@@ -1,6 +1,6 @@
 import Container from './styles/allPhoto'
 import Card from './card'
-import json from '../data2' //asserts { type: "json" }
+import json from '../data' //asserts { type: "json" }
 
 export default function FnAllPhoto() {
     const data = JSON.parse(json)
